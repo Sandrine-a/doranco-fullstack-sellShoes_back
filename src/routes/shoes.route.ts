a20a7922a8c238fad4ce65138d278c55740389f0
Sandrine-a <sandrine_a971@yahoo.fr>
2023-01-19 10:31:47 -0400

    Modification fonction post shoes

--- src/routes/shoes.route.ts
+++ src/routes/shoes.route.ts
@@ -22,47 +22,71 @@ export default async function shoes(app: FastifyInstance) {
       // 1 - Je vérifie de bien recevoir un jeton de connexion
       await request.jwtVerify()
 
-      // const decodedTokenToString = await request.jwtVerify()
-      // console.log("decodedTokenToString ==", JSON.stringify(decodedTokenToString));
+      // Je récupére et valide la nouvelle chaussure envoyé de la body
+      // de la request
+      const newShoes = NewShoesModel.parse(request.body)
 
-      // 2 - J'utilise le model User pour créer un schema avec uniquement l'_id et le mail
-      const userModelPartial = UserModel.pick({ _id: true, email: true })
-      // 3 - Je parse en userModelPartial la request.user qui contient les donnees user
-      const userValues = userModelPartial.parse(request.user)
-      //4- On recupere l'utilisateur tout juste enregistré
-      const userExist = await app.mongo.db?.collection('users').findOne({
-        _id: new ObjectId(userValues._id),
-        email: userValues.email,
+      // Récupérer l'identifiant de l'utilisateur contenue dans le jeton
+      // de connexion.
+      const userId = (request.user as any)._id
+      // Maintenant que je connais l'identifiant de l'utilisateur, j'utilise
+      // mongodb pour aller chercher l'utilisateur en question
+      const user = await app.mongo.db?.collection('users').findOne({
+        _id: new ObjectId(userId),
       })
-      if (userExist) {
-        // 5 - Je cree ma shoes pour l'enregistrer en database avec l'userId
-        // const newShoes = NewShoesModel.parse(request.body)
-        const newShoes = Object.assign(NewShoesModel.parse(request.body), {
-          user: userExist._id,
-        })
 
-        // 6 - On enregistre la chaussure en bdd
-        const result = await app.mongo.db
-          ?.collection('shoes')
-          .insertOne(newShoes)
+      // 6 - On enregistre la chaussure en bdd
+      const result = await app.mongo.db?.collection('shoes').insertOne({
+        ...newShoes,
+        user,
+      })
+      // 7- On recupere les shoes tout juste enregistrées
+      const shoes = await app.mongo.db?.collection('shoes').findOne({
+        _id: result?.insertedId,
+      })
+      // on ajoute le code reponse
+      response.code(201)
+      //On retourne la shoes validee par le model
+      return ShoesModel.parse(shoes)
 
-        // 7- On recupere l'utilisateur tout juste enregistré
-        const shoes = await app.mongo.db?.collection('shoes').findOne({
-          _id: result?.insertedId,
-        })
+      // // 1 - Je vérifie de bien recevoir un jeton de connexion
+      // await request.jwtVerify()
+      // // const decodedTokenToString = await request.jwtVerify()
+      // // console.log("decodedTokenToString ==", JSON.stringify(decodedTokenToString));
+      // // 2 - J'utilise le model User pour créer un schema avec uniquement l'_id et le mail
+      // const userModelPartial = UserModel.pick({ _id: true, email: true })
+      // // 3 - Je parse en userModelPartial la request.user qui contient les donnees user
+      // const userValues = userModelPartial.parse(request.user)
+      // //4- On recupere l'utilisateur tout juste enregistré
+      // const userExist = await app.mongo.db?.collection('users').findOne({
+      //   _id: new ObjectId(userValues._id),
+      //   email: userValues.email,
+      // })
+      // if (userExist) {
+      //   // 5 - Je cree ma shoes pour l'enregistrer en database avec l'userId
+      //   // const newShoes = NewShoesModel.parse(request.body)
+      //   const newShoes = Object.assign(NewShoesModel.parse(request.body), {
+      //     user: userExist._id,
+      //   })
+      //   // 6 - On enregistre la chaussure en bdd
+      //   const result = await app.mongo.db
+      //     ?.collection('shoes')
+      //     .insertOne(newShoes)
+      //   // 7- On recupere l'utilisateur tout juste enregistré
+      //   const shoes = await app.mongo.db?.collection('shoes').findOne({
+      //     _id: result?.insertedId,
+      //   })
+      //   // on ajoute le code reponse
+      //   response.code(201)
+      //   //On retourne la shoes validee par le model
+      //   return ShoesModel.parse(shoes)
+      // } else {
 
-        // console.log(shoes)
-        // on ajoute le code reponse
-        response.code(201)
-
-        //On retourne la shoes validee par le model
-        return ShoesModel.parse(shoes)
-      } else {
-        response.code(400)
-        return {
-          error: 'Bad request',
-        }
-      }
+      //   // response.code(400)
+      //   // return {
+      //   //   error: 'Bad request',
+      //   // }
+      // }
     },
   )
 }
